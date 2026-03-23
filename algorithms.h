e74ec6f569f5b0e4b3bb87a333e41bec77c4974d
Gayatri <gayatrisahani0316@gmail.com>
2026-03-24 02:14:11 +0530

    Add rCount to external variable declarations

--- algorithms.h
+++ algorithms.h
@@ -7,12 +7,13 @@
 #include <string>
 using namespace std;
 
-extern long long mCount, qCount, bCount, sCount, iCount;
+extern long long mCount, qCount, bCount, sCount, iCount, rCount;
 
 void mergeSort(vector<int>& arr, int l, int r);
 void quickSort(vector<int>& arr, int low, int high);
 void bubbleSort(vector<int>& arr);
 void selectionSort(vector<int>& arr);
 void insertionSort(vector<int>& arr);
+int binaryRecursion(int n);
 
 #endif
