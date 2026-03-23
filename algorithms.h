0c4937882a0408304d33a8a693b7dcf9e05781d4
Gayatri <gayatrisahani0316@gmail.com>
2026-03-23 22:52:00 +0530

    Refactor algorithms.h to declare sorting functions.

---
+++ algorithms.h
@@ -0,0 +1,18 @@
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+extern long long mCount, qCount, bCount, sCount, iCount;
+
+void mergeSort(vector<int>& arr, int l, int r);
+void quickSort(vector<int>& arr, int low, int high);
+void bubbleSort(vector<int>& arr);
+void selectionSort(vector<int>& arr);
+void insertionSort(vector<int>& arr);
+
+#endif
